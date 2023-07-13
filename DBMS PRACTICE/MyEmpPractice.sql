3ffc7223a315bd747ace875cdfe5db667099b7d2
KaleBhaskaraSrinivas <69496217+KaleBhaskaraSrinivas@users.noreply.github.com>
2023-07-13 21:31:52 +0530

    Add files via upload

--- DBMS PRACTICE/MyEmpPractice.sql
+++ DBMS PRACTICE/MyEmpPractice.sql
@@ -1,5 +1,35 @@
 -- Create table king_emp as select * from ramu_emp;
 
+-- CREATE TABLE king_emp (
+--   empno INT,
+--   ename VARCHAR(255),
+--   job VARCHAR(255),
+--   mgr INT,
+--   hiredate DATE,
+--   sal DECIMAL(8,2),
+--   comm DECIMAL(8,2),
+--   dept_no INT
+-- );
+
+-- INSERT INTO king_emp (empno, ename, job, mgr, hiredate, sal, comm, dept_no)
+-- VALUES
+--   (7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600.0, 300.0, 20),
+--   (7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250.0, 500.0, 30),
+--   (7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975.0, NULL, 20),
+--   (7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250.0, 1400.0, 30),
+--   (7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850.0, NULL, 30),
+--   (7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450.0, NULL, 10),
+--   (7788, 'SCOTT', 'ANALYST', 7566, '1982-12-09', 3000.0, NULL, 20),
+--   (7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', 5000.0, NULL, 10),
+--   (7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', 1500.0, 0.0, 30),
+--   (7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', 1100.0, NULL, 20),
+--   (7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950.0, NULL, 30),
+--   (7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000.0, NULL, 20),
+--   (7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300.0, NULL, 10),
+--   (1234, 'SMITH', 'CLERK', 7902, '1980-12-17', 800.0, NULL, 20),
+--   (7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600.0, 300.0, 30),
+--   (7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600.0, 300.0, 10)
+
 select * from king_emp;
 
 
@@ -276,28 +306,23 @@ select * from king_emp;
 -- ORDER BY lowest_salary;
 
 -- 47. Display all employee names, department numbers, and department names. 
--- (when there is dept table)
+
 -- SELECT
---   e.employee_name,
---   d.department_number,
---   d.department_name
+--   ename,
+--   dept_no
 -- FROM
---   employees e
--- JOIN
---   departments d ON e.department_id = d.department_id;
+--   king_emp;
 
 -- 48. Display all employee names and their department names in the order of their department 
 -- names. 
--- (when there is dept table)
+-- 
 -- SELECT
---   e.employee_name,
---   d.department_name
+-- ename,
+-- dept_no
 -- FROM
---   employees e
--- JOIN
---   departments d ON e.department_id = d.department_id
+--   king_emp
 -- ORDER BY
---   d.department_name;
+--   dept_no;
 
 -- 49. Display the department that has no employees. 
 
